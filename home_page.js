44c02725e0c0e6961954dd86e5f69f3be45a0557
yz-jpg <s10258968@connect.np.edu.sg>
2025-02-08 21:27:28 +0800

    Finished Home Page

---
+++ home_page.js
@@ -0,0 +1,8 @@
+  function toggleFilter() {
+    var panel = document.getElementById('filterPanel');
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  }
