bc9d3ba2c3202cd568ab719920836a82da67441b
Dylan <dylan.wongyk@gmail.com>
2025-02-09 13:41:47 +0800

    Changes

--- login_app.js
+++ login_app.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Redirect to the Lottie animation page.
         setTimeout(() => {
-          window.location.href = "redirect.html";
+          window.location.href = "redirect.html?email=" + encodeURIComponent(email);
         }, 2000);
       } catch (error) {
         console.error("Login error:", error);
